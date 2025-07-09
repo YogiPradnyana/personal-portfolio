791a78778a5d0f059af02bc98f6e86b8629d28d7
yogipradnyana <15tkj1yogipradnyana@gmail.com>
2025-07-09 20:07:21 +0800

    setup tailwind & font

---
+++ src/components/Hero.jsx
@@ -0,0 +1,8 @@
+// src/components/Hero.jsx
+import React from "react";
+
+function Hero() {
+  return <section className="hero-section text-pr">Hello World lorem</section>;
+}
+
+export default Hero;
